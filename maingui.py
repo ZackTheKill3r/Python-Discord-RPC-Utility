1c0dd9fb586b936f7e845c5b08809b3eefae132f
Kill3rZack <zackthekill3r@outlook.com>
2021-12-05 20:35:11 -0300

    The main RPC thing.
    Takes the inputs from the GUI and sends them to Discord RPC

---
+++ maingui.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+from pypresence import Presence
+import settings as cfg
+import time
+import asyncio
+import _thread
+import threading
+
+def main(button01,button01URL,button02,button02URL,state,details,buttonsenabled,buttonsdisabled):
+ client_id = cfg.client_id  # Enter your Application ID here.
+ RPC = Presence(client_id=client_id)
+ RPC.connect()
+ 
+ if buttonsenabled is True:
+     RPC.update(buttons=[{"label": f"{button01}", "url": f"{button01URL}"}, {"label": f"{button02}", "url": f"{button02URL}",}],state=f"{state}", details=f"{details}") # Can specify up to 2 buttons
+ elif buttonsdisabled is True:
+     RPC.update(state=f"{state}", details=f"{details}")
+
+ while 1:
+     time.sleep(15) #Can only update presence every 15 seconds
+
+
+
+     
