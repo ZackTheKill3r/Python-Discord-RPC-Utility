b4cc835d937c3327539ceaa3d8773bdad309fb51
Kill3rZack <zackthekill3r@outlook.com>
2021-12-05 20:36:05 -0300

    Where you set things up
    For now you only need to setup your "client_id" from your Discord Application

---
+++ settings.py
@@ -0,0 +1,7 @@
+#D.R.U Settings file
+#Here is where you set up stuff
+client_id = "" #go to https://discord.com/developers/applications , create an app and copy it's Application ID or Client ID, both should be the same
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#After setting this up everything should work perfectly out-of-the-box.
+#Version 0.1 Alpha
+#Made with <3 by ZackTheKill3r.
