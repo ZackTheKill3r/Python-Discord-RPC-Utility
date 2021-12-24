6f5fcb7c089d1ca960654e7f4692de2a0184a78d
Kill3rZack <zackthekill3r@outlook.com>
2021-12-23 22:36:13 -0300

    Delete settings.py

--- settings.py
+++
@@ -1,2 +0,0 @@
-#Client ID or Application ID is an number which you can get here https://discord.com/developers/applications just copy it and paste between those "" and you're good to go!
-client_id = "886728485979164682"
