702742dba661e2102803a122be94c43d118f750f
Kill3rZack <zackthekill3r@outlook.com>
2021-12-23 22:29:24 -0300

    Updating the version and not the file, because it's already good enough

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@
 client_id = "" #go to https://discord.com/developers/applications , create an app and copy it's Application ID or Client ID, both should be the same
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 #After setting this up everything should work perfectly out-of-the-box.
-#Version 0.1 Alpha
+#Version 2.0 Beta
 #Made with <3 by ZackTheKill3r.
