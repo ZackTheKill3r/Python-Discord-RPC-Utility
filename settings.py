dfda402b990a293eacd6c7724ef89d80cfd4062d
Kill3rZack <zackthekill3r@outlook.com>
2021-12-23 22:36:53 -0300

    new, less bloated settings file

---
+++ settings.py
@@ -0,0 +1,2 @@
+#Client ID or Application ID is an number which you can get here https://discord.com/developers/applications just copy it and paste between those "" and you're good to go!
+client_id = ""
