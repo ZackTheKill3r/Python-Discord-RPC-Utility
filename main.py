3db32c7cc38991e35ef83c4fd046bbf6c1e6ee77
Kill3rZack <zackthekill3r@outlook.com>
2021-12-25 01:07:57 -0300

    Beta 2.1 (Manual options)
    Now we have manual options, Translation to Spanish, general code cleaning and optimization, and better comments in the code.

--- main.py
+++ main.py
@@ -4,36 +4,44 @@ import languages as txt
 import settings as cfg
 import darkdetect
 
-#Detects if the user is using Light Theme or Dark Theme
-if darkdetect.isDark() is True:
+#Checks if ManualThemes are enabled and chooses the preferred theme, else it just detects the OS's current theme.
+if cfg.ManualTheme is True:
+    theme = cfg.Theme
+elif darkdetect.isDark() is True:
+    theme = "Dark"
+elif darkdetect.isDark() is False:
+    theme = "Light"
+
+#Dark Theme palette.
+if "Dark" in theme:
     #Dark Theme
     sg.theme_background_color("#4d4d4d")
     sg.theme_text_element_background_color("#4d4d4d")
     sg.theme_element_background_color("#4d4d4d")
     sg.theme_button_color("#1a1a1a")
-elif darkdetect.isLight() is True:
-    #Light Theme
+#Light Theme palette
+elif "Light" in theme:
     sg.theme_background_color("#dedede")
     sg.theme_text_element_background_color("#dedede")
     sg.theme_text_color("#000000")
     sg.theme_element_background_color("#dedede")
     sg.theme_button_color("#1a1a1a")
 
-#Presence
+#Presence (the main RPC updater)
 def presence(title,description,button_enabled,top_title,top_url,bottom_title,bottom_url):
     from pypresence import Presence
     client_id = cfg.client_id
     RPC = Presence(client_id=client_id)
     RPC.connect()
     if button_enabled is True:
-     RPC.update(buttons=[{"label": top_title, "url": top_url}, {"label": bottom_title, "url": bottom_url}],state=title,details=description) # Can specify up to 2 buttons
+     RPC.update(buttons=[{"label": top_title, "url": top_url}, {"label": bottom_title, "url": bottom_url}],state=description,details=title) #Presence **with** Buttons.
     else:
-        RPC.update(state=title, details=description)
+        RPC.update(state=description, details=title) #Presence without buttons.
 
     while 1:
-     time.sleep(15) #Can only update presence every 15 seconds
+     time.sleep(15) #Can only update presence every 15 seconds (Presence loop).
 
-#Main Shit
+#Main GUI Function
 def main():
     layout = [  [sg.Text(txt.Title,)],
                 [sg.Input(key='title')],
@@ -51,10 +59,9 @@ def main():
                 [sg.Input(key='bottom_url')],
                 [sg.Button(txt.Okay), sg.Button(txt.Cancel)]  ]
                 
-
     window = sg.Window('Discord RPC Utility', layout)
 
-    while True: # Event Loop
+    while True: # GUI Event Loop
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
@@ -72,12 +79,8 @@ def main():
             bottom_title=""
             bottom_url=""
 
-
-        if event == txt.Okay:
-            print("okay boomer")
-            print(f"{title}")
-            print(button_enabled)
-            window.perform_long_operation(presence(title,description,button_enabled,top_title,top_url,bottom_title,bottom_url), '-OPERATION DONE-')
+        if event == txt.Okay: 
+            window.perform_long_operation(presence(title,description,button_enabled,top_title,top_url,bottom_title,bottom_url)) #Calls the presence function without lagging the GUI itself.
         elif event  == txt.Cancel:
             break
 
