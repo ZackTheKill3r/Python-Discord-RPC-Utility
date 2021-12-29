5182a8f420270823a8437a57daf165c576e20b90
Kill3rZack <zackthekill3r@outlook.com>
2021-12-29 19:59:07 -0300

    this shoudn't be uploaded..... oops

--- test.py
+++
@@ -1,9 +0,0 @@
-""" lists environment variables, and splits elements in path variable """
-import os
-
-
-for k, v in sorted(os.environ.items()):
-    print(k+':', v)
-print('\n')
-# list elements in path environment variable
-[print(item) for item in os.environ['PATH'].split(';')]
